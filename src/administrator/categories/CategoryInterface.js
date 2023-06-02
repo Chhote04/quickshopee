73026a12182c87586fc29b07aa17ca6d44c87cf1
Chhotelaljatav143@gmail.com <satishsuman111@gmail.com>
2023-06-02 17:34:56 +0530

    Day 2 completed

--- src/administrator/categories/CategoryInterface.js
+++ src/administrator/categories/CategoryInterface.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { makeStyles } from '@mui/styles';
-import { Grid, TextField, Button, FormControl, InputLabel, MenuItem, IconButton, Select } from "@mui/material"
+import { Avatar, Grid, TextField, Button, FormControl, InputLabel, MenuItem, IconButton, Select } from "@mui/material"
 import { PhotoCamera } from "@mui/icons-material";
 const useStyles = makeStyles({
     container: {
@@ -16,17 +16,33 @@ const useStyles = makeStyles({
         height: 'auto',
         padding: 10,
         background: '#ffff'
+    },
+    headingStyle: {
+        fontFamily: 'poppinns',
+        fontWeight: 600,
+        letterSpacing: 1,
+        fontSize: 18
     }
 })
 
 export default function CategoryInterface() {
     const classes = useStyles()
     const [status, setStatus] = useState('')
-    return (<div className={classes.container}>
+    const [icon, setIcon] = useState({ file: '/assets/icons/shopping-cart.png', bytes: '' })
+    const [categoryName, setCategoryName] = useState('')
+    const handlePicture = (event) => {
+
+        setIcon({ file: URL.createObjectURL(event.target.files[0]), bytes: event.target.files[0] })
+
+    }
+    return (<div className={classes.container} >
         <div className={classes.box}>
-            <Grid container spacing={3}>
+            <Grid container spacing={2} >
                 <Grid item xs={12}>
-                    <TextField label="Categoty Name" variant="outlined" fullWidth />
+                    <div categoryName={classes.headingStyle}>Add New Category</div>
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField onChange={(event) => { setCategoryName(event.target.value) }} label="Categoty Name" variant="outlined" fullWidth />
                 </Grid>
                 <Grid item xs={12}>
                     <FormControl fullWidth>
@@ -37,13 +53,23 @@ export default function CategoryInterface() {
                         </Select>
                     </FormControl>
                 </Grid>
-                <Grid item xs={12}>
+                <Grid item xs={6}>
                     <IconButton color="primary" aria-label="upload picture" component="label">
-                        <input hidden accept="image/*" type="file" />
+                        <input onChange={handlePicture} hidden accept="image/*" type="file" />
                         <PhotoCamera />
                     </IconButton>
                 </Grid>
+                <Grid item xs={6}>
+                    <Avatar alt="Icon" variant="rounded" src={icon.file} sx={{ width: 56, height: 56 }} />
+                </Grid>
+                <Grid item xs={6}>
+                    <Button variant='contained' fullWidth>Submt</Button>
+                </Grid>
+                <Grid item xs={6}>
+                    <Button variant='contained' fullWidth>Reset</Button>
+                </Grid>
             </Grid>
+
 
         </div>
     </div>)
